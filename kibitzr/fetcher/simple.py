c8bcac29c06dfd148e37872d1c45a55dc5367eaa
Martin Virtel <martin.virtel@gmail.com>
2017-04-18 09:00:07 +0200

    enable conditional get

--- kibitzr/fetcher/simple.py
+++ kibitzr/fetcher/simple.py
@@ -1,7 +1,10 @@
 from time import sleep
 import requests
-
+import logging
 from kibitzr import __version__ as version
+
+
+logger = logging.getLogger(__name__)
 
 
 class SessionFetcher(object):
@@ -13,12 +16,18 @@ class SessionFetcher(object):
         })
         self.url = conf['url']
         self.valid_http = set(conf.get('valid_http', [200]))
+        self.last_headers = {'last-modified': False, 'etag': False}
 
     def fetch(self, *args, **_kwargs):
         retries = 3
+        headers = {}
+        if self.last_headers["last-modified"]:
+            headers["If-Modified-Since"] = self.last_headers["last-modified"]
+        elif self.last_headers["etag"]:
+            headers["If-None-Match"] = self.last_headers["etag"]
         for retry in range(retries):
             try:
-                response = self.session.get(self.url)
+                response = self.session.get(self.url, headers=headers)
             except requests.HTTPError:
                 if retry == retries - 1:
                     raise
@@ -31,5 +40,19 @@ class SessionFetcher(object):
                 else:
                     sleep(15)
                     continue
+            if response.status_code == 200:
+                for k in self.last_headers.keys():
+                    v = response.headers.get(k, False)
+                    if v:
+                        self.last_headers[k] = v
+                        logger.debug(
+                           "{self.url} - {k}: {v}".format(**locals())
+                        )
+            elif response.status_code == 304:
+                    logger.debug(
+                        "{self.url} - {response.status_code}".format(
+                         **locals()
+                         )
+                    )
             ok = (response.status_code in self.valid_http)
             return ok, response.text
