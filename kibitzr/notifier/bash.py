509d67e7501227a3cd4c2ed2eb35f2671230a284
Martin Virtel <martin.virtel@gmail.com>
2017-05-13 09:38:56 +0200

    log bash notification with name

--- kibitzr/notifier/bash.py
+++ kibitzr/notifier/bash.py
@@ -1,14 +1,20 @@
 from ..bash import execute_bash
+import logging
 
+logger = logging.getLogger(__name__)
 
 class BashNotify(object):
 
-    def __init__(self, value):
+    def __init__(self, value, conf):
         self.code = value
+        self.conf = conf
 
     def __call__(self, report):
-        execute_bash(self.code, report)
+        rv=execute_bash(self.code, report)
+        if rv[0] :
+            logger.info("Notified {} via bash".format(self.conf.get("name","")))
 
 
 def notify_factory(conf, value):
-    return BashNotify(value)
+    return(BashNotify(value,conf))
+
